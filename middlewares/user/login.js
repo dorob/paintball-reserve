29abbb2d6f0dad1f097ff04f8bf6d993f80d3766
AttilaDoro <doroattila@gmail.com>
2018-05-19 23:55:35 +0200

    fix login error in case of mongo connection error

--- middlewares/user/login.js
+++ middlewares/user/login.js
@@ -7,7 +7,7 @@ module.exports = function (objectrepository) {
     return function (req, res, next) {
         if ((typeof req.body === 'undefined') || (typeof req.body.email === 'undefined') || (typeof req.body.password === 'undefined')) {
             res.tpl.error.push("Felhasználónév vagy jelszó érvénytelen formátumú");
-            res.redirect('/login');
+            return res.redirect('/login');
         }
 
         userModel.findOne({
@@ -15,17 +15,17 @@ module.exports = function (objectrepository) {
             password: req.body.password
         },
             function (err, result) {
-                if ((err) || (result === null)) {
+                if (err || !result) {
                     res.tpl.error.push("A felhasználónév vagy jelszó nem helyes");
-                    res.redirect('/login');
+                    return res.redirect('/login');
                 }
                 else {
                     req.session.userid = result._id;
                     req.session.email = result.email;
                     if (result.hasAdminRight)
                         req.session.hasAdmin = true;
+                    return res.redirect('/');
                 }
-                res.redirect('/');
             });
     };
 };
